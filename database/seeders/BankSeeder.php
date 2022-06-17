210bd1988791abdae0a78291b559b012b189f663
dinafauzyh <dfauziyah18@gmail.com>
2022-06-17 23:02:39 +0700

    Add model, migration, and seeder for Bank

---
+++ database/seeders/BankSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BankSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('banks')->insert([
+            [
+                'name' => 'Mandiri'
+            ],
+
+            [
+                'name' => 'BCA'
+            ]
+        ]);
+    }
+}
